fefc1c97d7a344f2cae454591ad525abbe8cb508
gfdev1209 <gf99447312@gmail.com>
2021-05-10 20:22:13 -0600

    Fix bug with alert panel height. Updated icon styling and date formatting.

--- src/app/map/services/map.service.ts
+++ src/app/map/services/map.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { AlertType } from '../enums';
 import { Alert } from '../models';
 
