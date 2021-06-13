93f600f14a7150fac359586ae24b71d854a718ee
gfdev1209 <gf99447312@gmail.com>
2021-06-13 16:58:46 -0600

    Complete styling reporting data table

--- src/app/map/services/alert.service.ts
+++ src/app/map/services/alert.service.ts
@@ -1,9 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Observable, of, throwError } from 'rxjs';
-import { delay } from 'rxjs/operators';
-import { AlertType } from '@map/enums';
-import { Alert } from '@map/models';
-import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { BaseService } from '@shared/services/base.service';
 
@@ -14,11 +9,4 @@ export class AlertService extends BaseService {
   constructor(private httpClient: HttpClient) {
     super('alerts', httpClient);
   }
-
-  // getAll(): Observable<Alert[]> {
-  //   return this.httpClient.get<Alert[]>(`${this.baseUrl}api/alerts`);
-  // }
-  // get(id: number): Observable<Alert> {
-  //   return this.httpClient.get<Alert>(`${this.baseUrl}api/alerts/${id}`);
-  // }
 }
--- src/app/report/components/alert-table/alert-table.component.ts
+++ src/app/report/components/alert-table/alert-table.component.ts
@@ -5,6 +5,9 @@ import { RootState } from '@store/index';
 import * as AlertActions from '@store/alert/alert.actions';
 import * as AlertSelectors from '@store/alert/alert.selectors';
 
+import * as BuildingActions from '@store/building/building.actions';
+import * as BuildingSelectors from '@store/building/building.selectors';
+
 @Component({
   selector: 'app-alert-table',
   templateUrl: './alert-table.component.html',
@@ -12,10 +15,12 @@ import * as AlertSelectors from '@store/alert/alert.selectors';
 })
 export class AlertTableComponent implements OnInit {
   alerts$ = this.store.select(AlertSelectors.selectAll);
+  buildings$ = this.store.select(BuildingSelectors.selectAll);
 
   constructor(private store: Store<RootState>) {}
 
   ngOnInit(): void {
     this.store.dispatch(AlertActions.getAll());
+    this.store.dispatch(BuildingActions.getAll());
   }
 }
