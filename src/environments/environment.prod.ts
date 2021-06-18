d764fced26716d0bd7d408b0866b0c17584920ad
gfdev1209 <gf99447312@gmail.com>
2021-06-17 22:55:28 -0400

    Update stuartshome env and add apitest env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,18 +1,18 @@
 export const environment = {
   production: true,
-  apiUrl: '',
+  apiUrl: 'https://api.stuartshome.airia20.com/api/',
   // MapBox
   mapboxAccessToken:
     'pk.eyJ1IjoibWlrZWFpcmlhIiwiYSI6ImNrbnF1cnNnaTBnaG8ydm15dXRuOGVodDgifQ.LbEjDzKyUje8uRE220hoqQ',
   // Azure B2C Configuration
-  b2cClientId: '0d65b9b8-d654-481f-95d1-244fa3e6275b',
+  b2cClientId: '3696fd50-5745-45f7-b4d2-25fbaba1d90e',
   b2cNameSignUpSignIn: 'B2C_1_airiasignupin',
   b2cNameEditProfile: 'B2C_1_airiaprofile',
-  b2cAuthorityDomain: 'airiastuartshome.b2clogin.com',
+  b2cAuthorityDomain: 'apitest.b2clogin.com',
   b2cAuthoritySignUpSignIn:
-    'https://airiastuartshome.b2clogin.com/stuartshome.airia20.com/B2C_1_airiasignupin',
+    'https://apitest.b2clogin.com/stuartshome.airia20.com/B2C_1_airiasignupin',
   b2cAuthorityEditProfile:
-    'https://airiastuartshome.b2clogin.com/stuartshome.airia20.com/B2C_1_airiaprofile',
-  b2cScopes: ['https://stuartshome.airia20.com/api/demo.read'],
-  b2cApiUrl: 'https://api.stuartshome.airia20.com',
+    'https://apitest.b2clogin.com/stuartshome.airia20.com/B2C_1_airiaprofile',
+  b2cScopes: ['https://apitest.airia20.com/api/demo.read'],
+  b2cApiUrl: 'https://api.apitest.airia20.com',
 };
