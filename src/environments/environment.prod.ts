aedc57b246f06311062eacdd0886cee8146ce31d
Stuart Anderson <60015542+stuanderson@users.noreply.github.com>
2021-06-18 05:48:38 -0400

    Update environment.prod.ts
    Fixing issues caused by apitest changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,14 +5,14 @@ export const environment = {
   mapboxAccessToken:
     'pk.eyJ1IjoibWlrZWFpcmlhIiwiYSI6ImNrbnF1cnNnaTBnaG8ydm15dXRuOGVodDgifQ.LbEjDzKyUje8uRE220hoqQ',
   // Azure B2C Configuration
-  b2cClientId: '3696fd50-5745-45f7-b4d2-25fbaba1d90e',
+  b2cClientId: '0d65b9b8-d654-481f-95d1-244fa3e6275b',
   b2cNameSignUpSignIn: 'B2C_1_airiasignupin',
   b2cNameEditProfile: 'B2C_1_airiaprofile',
-  b2cAuthorityDomain: 'apitest.b2clogin.com',
+  b2cAuthorityDomain: 'airiastuartshome.b2clogin.com',
   b2cAuthoritySignUpSignIn:
-    'https://apitest.b2clogin.com/stuartshome.airia20.com/B2C_1_airiasignupin',
+    'https://airiastuartshome.b2clogin.com/stuartshome.airia20.com/B2C_1_airiasignupin',
   b2cAuthorityEditProfile:
-    'https://apitest.b2clogin.com/stuartshome.airia20.com/B2C_1_airiaprofile',
-  b2cScopes: ['https://apitest.airia20.com/api/demo.read'],
-  b2cApiUrl: 'https://api.apitest.airia20.com',
+    'https://airiastuartshome.b2clogin.com/stuartshome.airia20.com/B2C_1_airiaprofile',
+  b2cScopes: ['https://stuartshome.airia20.com/api/demo.read'],
+  b2cApiUrl: 'https://api.stuartshome.airia20.com',
 };
