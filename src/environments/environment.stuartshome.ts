371f68ec363e4ade37e839162757068aaa454c96
Stuart Anderson <60015542+stuanderson@users.noreply.github.com>
2021-06-18 05:45:21 -0400

    Update environment.stuartshome.ts
    Fix stuartshome configuration issues

--- src/environments/environment.stuartshome.ts
+++ src/environments/environment.stuartshome.ts
@@ -8,11 +8,11 @@ export const environment = {
   b2cClientId: '0d65b9b8-d654-481f-95d1-244fa3e6275b',
   b2cNameSignUpSignIn: 'B2C_1_airiasignupin',
   b2cNameEditProfile: 'B2C_1_airiaprofile',
-  b2cAuthorityDomain: 'stuartshome.b2clogin.com',
+  b2cAuthorityDomain: 'airiastuartshome.b2clogin.com',
   b2cAuthoritySignUpSignIn:
-    'https://stuartshome.b2clogin.com/stuartshome.airia20.com/B2C_1_airiasignupin',
+    'https://airiastuartshome.b2clogin.com/stuartshome.airia20.com/B2C_1_airiasignupin',
   b2cAuthorityEditProfile:
-    'https://stuartshome.b2clogin.com/stuartshome.airia20.com/B2C_1_airiaprofile',
+    'https://airiastuartshome.b2clogin.com/stuartshome.airia20.com/B2C_1_airiaprofile',
   b2cScopes: ['https://stuartshome.airia20.com/api/demo.read'],
   b2cApiUrl: 'https://api.stuartshome.airia20.com',
 };
