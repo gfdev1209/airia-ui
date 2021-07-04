bafe56bce87303470c94ed185ed86e3d4fcd966d
Mike Wojcik <mike@likwidgames.com>
2021-07-04 16:16:21 -0600

    Initial User Form

--- src/app/settings/views/user-form-view/user-form-view.component.ts
+++ src/app/settings/views/user-form-view/user-form-view.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from '@map/models';
 
 @Component({
@@ -6,10 +13,28 @@ import { User } from '@map/models';
   templateUrl: './user-form-view.component.html',
   styleUrls: ['./user-form-view.component.scss'],
 })
-export class UserFormViewComponent implements OnInit {
+export class UserFormViewComponent implements OnInit, OnChanges {
   @Input() user?: User | null;
 
-  constructor() {}
+  userForm!: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(private fb: FormBuilder) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.user && !changes.user.firstChange) {
+      this.userForm.patchValue(changes.user.currentValue);
+    }
+  }
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      firstName: [this.user?.firstName, Validators.required],
+      lastName: [this.user?.lastName, Validators.required],
+      email: [this.user?.email, [Validators.required, Validators.email]],
+      phone: [
+        this.user?.phone,
+        [Validators.required, Validators.minLength(12)],
+      ],
+    });
+  }
 }
