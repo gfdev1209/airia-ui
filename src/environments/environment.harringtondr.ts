57585de15fada4fb5c8a27ff0fa9ed21c56dad7d
stuartanderson <stuart@andersonfamily.eu>
2021-07-07 20:50:55 -0400

    Correct clientID for harringtondr

--- src/environments/environment.harringtondr.ts
+++ src/environments/environment.harringtondr.ts
@@ -5,7 +5,7 @@ export const environment = {
   mapboxAccessToken:
     'pk.eyJ1IjoibWlrZWFpcmlhIiwiYSI6ImNrbnF1cnNnaTBnaG8ydm15dXRuOGVodDgifQ.LbEjDzKyUje8uRE220hoqQ',
   // Azure B2C Configuration
-  b2cClientId: '0d65b9b8-d654-481f-95d1-244fa3e6275b',
+  b2cClientId: '07a24a28-4224-438b-a4a8-e386247a50f1',
   b2cNameSignUpSignIn: 'B2C_1_airiasignupin',
   b2cNameEditProfile: 'B2C_1_airiaprofile',
   b2cAuthorityDomain: 'airiaharringtondr.b2clogin.com',
