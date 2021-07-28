94555b095174a15069555cac8f3d905315df2c7e
Mike Wojcik <mike@likwidgames.com>
2021-07-27 21:17:44 -0600

    Log out confirmation modal

--- src/app/shared/components/left-nav/left-nav.component.ts
+++ src/app/shared/components/left-nav/left-nav.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { MsalService } from '@azure/msal-angular';
+import { ConfirmationService, ConfirmEventType } from 'primeng/api';
 
 @Component({
   selector: 'app-left-nav',
   templateUrl: './left-nav.component.html',
   styleUrls: ['./left-nav.component.scss'],
+  providers: [ConfirmationService],
 })
 export class LeftNavComponent implements OnInit {
   expanded = false;
 
-  constructor(private authService: MsalService) {}
+  constructor(
+    private authService: MsalService,
+    private confirmationService: ConfirmationService
+  ) {}
 
   ngOnInit(): void {}
 
@@ -18,6 +23,15 @@ export class LeftNavComponent implements OnInit {
   }
 
   logout(): void {
-    this.authService.logoutRedirect({ postLogoutRedirectUri: '/login' });
+    this.confirmationService.confirm({
+      message: `Are you sure that you want to log out of Airia?`,
+      header: 'Log Out',
+      acceptButtonStyleClass: 'p-mr-0',
+      acceptIcon: 'fal fa-check',
+      rejectIcon: 'fal fa-times',
+      accept: () => {
+        this.authService.logoutRedirect({ postLogoutRedirectUri: '/map' });
+      },
+    });
   }
 }
