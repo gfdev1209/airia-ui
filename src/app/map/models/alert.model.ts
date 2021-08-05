b90522efc067c8ab1aebd9ed288bf776f63494ca
Mike Wojcik <mike@likwidgames.com>
2021-08-05 15:54:49 -0600

    Bug fixes for underscore pipe, alert preview, floor form

--- src/app/map/models/alert.model.ts
+++ src/app/map/models/alert.model.ts
@@ -16,16 +16,16 @@ export class Alert implements IBase {
     buildingId: number;
     accessPointId: number;
     alertSeverity: number;
-    type: string;
-    message: string;
+    alertType: string;
+    alertMessage: string;
     createdAt: Date;
   }) {
     this.id = args.alertId;
     this.buildingId = args.buildingId;
     this.accessPointId = args.accessPointId;
     this.severity = args.alertSeverity as AlertSeverity;
-    this.type = args.type as AlertType;
-    this.message = args.message;
+    this.type = args.alertType as AlertType;
+    this.message = args.alertMessage;
     this.createdAt = args.createdAt;
   }
 }
