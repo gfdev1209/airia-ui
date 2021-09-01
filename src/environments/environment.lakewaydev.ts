4c7c5e0ae2eb144cdd2e27ac9acde7ce732aa8f0
stuartanderson <stuart@andersonfamily.eu>
2021-09-01 13:14:28 -0400

    Fix lakeway API URL

--- src/environments/environment.lakewaydev.ts
+++ src/environments/environment.lakewaydev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://lakewaydev-api.dev.airia20.com/api/',
+  apiUrl: 'https://lakeway-api.dev.airia20.com/api/',
   // MapBox
   mapboxAccessToken:
     'pk.eyJ1IjoibWlrZWFpcmlhIiwiYSI6ImNrbnF1cnNnaTBnaG8ydm15dXRuOGVodDgifQ.LbEjDzKyUje8uRE220hoqQ',
@@ -14,5 +14,5 @@ export const environment = {
   b2cAuthorityEditProfile:
     'https://airialakewaydevelop.b2clogin.com/lakewaydev.airia20.com/B2C_1_airiaprofile',
   b2cScopes: ['https://lakewaydev.airia20.com/api/demo.read'],
-  b2cApiUrl: 'https://lakewaydev-api.dev.airia20.com',
+  b2cApiUrl: 'https://lakeway-api.dev.airia20.com',
 };
