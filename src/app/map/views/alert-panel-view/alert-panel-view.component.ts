a7a79c2087efa991e7d478827e0bca590985861a
Mike Wojcik <mike@likwidgames.com>
2021-11-12 11:05:35 -0700

    Fix bug to fliter acknowledged alerts

--- src/app/map/views/alert-panel-view/alert-panel-view.component.ts
+++ src/app/map/views/alert-panel-view/alert-panel-view.component.ts
@@ -140,7 +140,7 @@ export class AlertPanelViewComponent implements OnInit, OnChanges {
       : alert?.alertSeverity !== AlertSeverity.Low;
   };
   filterAcknowledged = (alert: Alert) => {
-    return this.showAcknowledged ? true : alert?.acknowledgedAt === null;
+    return this.showAcknowledged ? true : alert?.acknowledgedAt === undefined;
   };
   // filterCapacity = (alert: Alert) => {
   //   return this.showCapacity
