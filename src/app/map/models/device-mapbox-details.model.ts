1f5f69b852aa69cea588401738b337f4533fd57f
Mike Wojcik <mike@likwidgames.com>
2022-01-18 16:39:04 -0700

    Cluster devices and display count on hover

--- src/app/map/models/device-mapbox-details.model.ts
+++ src/app/map/models/device-mapbox-details.model.ts
@@ -1,8 +1,10 @@
 export class DeviceMapboxDetails {
   id!: string;
+  layers!: string[];
   sourceName!: string;
   heatmapName!: string;
   circleColor!: string;
   circleRadius!: number;
+  circleOpacity!: number;
   heatmapColorRGB!: string;
 }
