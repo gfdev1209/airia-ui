f82b0befdf42654c23aaa7ad212cc30d00e51799
gfdev1209 <gf99447312@gmail.com>
2022-02-09 01:18:54 -0500

    Add ncidev profile

---
+++ src/environments/environment.ncidev.ts
@@ -0,0 +1,24 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://nci-api.dev.airia20.com/api/',
+  // Client
+  timeZone: 'America/New_York',
+  timeZoneOffsetUTC: -5,
+  // MapBox
+  mapboxAccessToken:
+    'pk.eyJ1IjoibWlrZWFpcmlhIiwiYSI6ImNrbnF1cnNnaTBnaG8ydm15dXRuOGVodDgifQ.LbEjDzKyUje8uRE220hoqQ',
+  zoomLevel: 16,
+  hidePOIs: false,
+  hideStreetLabels: false,
+  // Azure B2C Configuration
+  b2cClientId: '1ee073b5-4b2a-4a6d-bee4-30776a0fbd21',
+  b2cNameSignUpSignIn: 'B2C_1_airiasignupin',
+  b2cNameEditProfile: 'B2C_1_airiaprofile',
+  b2cAuthorityDomain: 'airiancidevelop.b2clogin.com',
+  b2cAuthoritySignUpSignIn:
+    'https://airiancidevelop.b2clogin.com/ncidev.airia20.com/B2C_1_airiasignupin',
+  b2cAuthorityEditProfile:
+    'https://airiancidevelop.b2clogin.com/ncidev.airia20.com/B2C_1_airiaprofile',
+  b2cScopes: ['https://ncidev.airia20.com/api/demo.read'],
+  b2cApiUrl: 'https://nci-api.dev.airia20.com',
+};
