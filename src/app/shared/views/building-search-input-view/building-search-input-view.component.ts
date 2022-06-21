bbc0d3c083a72f316525b119edbddb75826992d0
gfdev1209 <gf99447312@gmail.com>
2022-06-21 15:16:10 -0500

    drop down click issue

--- src/app/shared/views/building-search-input-view/building-search-input-view.component.ts
+++ src/app/shared/views/building-search-input-view/building-search-input-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { Building } from '@map/models';
 
 @Component({
@@ -7,6 +7,7 @@ import { Building } from '@map/models';
   styleUrls: ['./building-search-input-view.component.scss'],
 })
 export class BuildingSearchInputViewComponent implements OnInit {
+  @ViewChild('autoComplete') autoComplete:ElementRef<any> | undefined;
   @Input() searchResults?: Building[] | null;
   @Output() search = new EventEmitter<string>();
   @Output() selectBuilding = new EventEmitter<Building>();
@@ -22,5 +23,26 @@ export class BuildingSearchInputViewComponent implements OnInit {
   }
   onSelect(building: Building): void {
     this.selectBuilding.emit(building);
+  }
+
+  onClear(event:any){
+    console.log("onClear", event);
+  }
+
+  handleDropdown(event:any){
+    const results = this.searchResults?.map((_arrayElement) => Object.assign({}, _arrayElement));
+    console.log("searchResults", this.searchResults);
+    this.searchResults = [];
+    this.searchResults = results;
+
+    event.originalEvent.preventDefault();
+    event.originalEvent.stopPropagation();
+    if (this.autoComplete?.nativeElement?.panelVisible) {
+      this.autoComplete?.nativeElement?.onDropdownBlur();
+      this.autoComplete.nativeElement?.hide();
+    } else {
+      this.autoComplete?.nativeElement?.onDropdownFocus();
+      this.autoComplete?.nativeElement?.show();
+    }
   }
 }
