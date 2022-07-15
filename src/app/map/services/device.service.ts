00ca0d34713261dc230d16678e6a76f109b9d87a
gfdev1209 <gf99447312@gmail.com>
2022-07-15 17:05:11 -0400

    implement full fetch of paged data for date range device query

--- src/app/map/services/device.service.ts
+++ src/app/map/services/device.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BaseService } from '@shared/services/base.service';
 import { Device } from '@map/models';
-import { Observable } from 'rxjs';
-import { retry, map, catchError, share, debounceTime } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import {
+  retry, map, catchError, share, debounceTime, concatMap
+} from 'rxjs/operators';
 import * as moment from 'moment-timezone';
 import Helpers from '@core/utils/helpers';
 
@@ -50,12 +52,32 @@ export class DeviceService extends BaseService {
       .pipe(
         debounceTime(1000),
         retry(2),
-        map((response: any) => this.mapArrayResponseToObject(response)),
+        concatMap((response: any) => this.getSelfThenNext(response, [])),
         catchError((error) => {
           return this.handleError(error);
         }),
         share()
       );
+  }
+
+  getSelfThenNext(response: any, devices: Device[]): Observable<Device[]> {
+    if (response._embedded === null ||
+        response._links === null) {
+      throw new Error('Invalid parameter to getSelfThenNext');
+    }
+
+    let nextDevices = devices.concat(response._embedded
+      .records
+      .map((responseJson: any) => new Device(responseJson))
+    );
+
+    if (response._links.next) {
+      return this.http.get(response._links.next).pipe(
+        concatMap(x => this.getSelfThenNext(x, nextDevices))
+      );
+    } else {
+      return of(nextDevices);
+    }
   }
 
   mapResponseToObject<T>(response: any): T {
